41944e0b979e8385a7644f6d33609233da3a0d1e
sangwkim <sangwoon@mit.edu>
2022-10-06 14:07:29 -0400

    check

---
+++ custom-factors/cpp/greeting.h
@@ -0,0 +1,28 @@
+#include <gtsam/geometry/Rot3.h>
+#include <gtsam/linear/NoiseModel.h>
+
+#include <iostream>
+#include <string>
+
+namespace gtsam_custom_factors {
+
+class Greeting {
+ public:
+  gtsam::noiseModel::Base::shared_ptr model =
+      gtsam::noiseModel::Isotropic::Sigma(1, 0.1);
+
+  /// Print a greeting
+  void sayHello() const;
+
+  /// Process a GTSAM object
+  gtsam::Rot3 invertRot3(gtsam::Rot3 rot) const;
+
+  /// Print a farewell
+  void sayGoodbye() const;
+
+  gtsam::Matrix getMatrix(gtsam::noiseModel::Base::shared_ptr model) const {
+    return gtsam::Matrix3::Zero();
+  }
+};
+
+}
--- custom-factors/python/gtasm_custom_factors/__init__.py
+++
@@ -1,2 +0,0 @@
-import gtsam
-from .gtasm_custom_factors import *
