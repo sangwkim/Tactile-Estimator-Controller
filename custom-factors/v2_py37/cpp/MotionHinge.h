b0dd7cf4ff6fabb34acd5062e9dc41e07e302847
sangwoonLenovo <sangwoon@mit.edu>
2023-03-20 17:11:45 +0900

    MotionHinge.h checkpoint

--- custom-factors/v2_py37/cpp/MotionHinge.h
+++ custom-factors/v2_py37/cpp/MotionHinge.h
@@ -6,56 +6,46 @@
 
 namespace gtsam_custom_factors {
 
-class MotionHinge: public gtsam::NoiseModelFactor4<gtsam::Pose3, gtsam::Pose3, gtsam::Pose3, gtsam::Pose3> {
-
-private:
-
-    gtsam::Point3 oc;
+class MotionHinge: public gtsam::NoiseModelFactor3<gtsam::Pose3, gtsam::Pose3, gtsam::Pose3> {
 
 public:
 
-  MotionHinge(gtsam::Key key1, gtsam::Key key2, gtsam::Key key3, gtsam::Key key4, const gtsam::Point3 contact_point, double cost_sigma) :
-      gtsam::NoiseModelFactor4<gtsam::Pose3, gtsam::Pose3, gtsam::Pose3, gtsam::Pose3>(gtsam::noiseModel::Isotropic::Sigma(1, cost_sigma), key1, key2, key3, key4),
-      oc(contact_point) {}
+  MotionHinge(gtsam::Key key1, gtsam::Key key2, gtsam::Key key3, double cost_sigma) :
+      gtsam::NoiseModelFactor3<gtsam::Pose3, gtsam::Pose3, gtsam::Pose3>(gtsam::noiseModel::Isotropic::Sigma(1, cost_sigma), key1, key2, key3) {}
 
   gtsam::Vector evaluateError(
             const gtsam::Pose3& pg,
-            const gtsam::Pose3& po,
             const gtsam::Pose3& pc,
             const gtsam::Pose3& pG,
             boost::optional<gtsam::Matrix&> Hg = boost::none,
-            boost::optional<gtsam::Matrix&> Ho = boost::none,
             boost::optional<gtsam::Matrix&> Hc = boost::none,
             boost::optional<gtsam::Matrix&> HG = boost::none) const {
 
-            typename gtsam::traits<gtsam::Pose3>::ChartJacobian::Jacobian HG_, HGgo;
-            typename gtsam::Matrix36 HcGgo;
+            typename gtsam::traits<gtsam::Pose3>::ChartJacobian::Jacobian HG_, HGgc;
+            typename gtsam::Matrix36 HcGgc;
             gtsam::Matrix Hy = (gtsam::Matrix13() << 0,1,0).finished();
 
-            gtsam::Pose3 pco = gtsam::traits<gtsam::Pose3>::Between(pc,po);
-            gtsam::Pose3 pgo = gtsam::traits<gtsam::Pose3>::Between(pg,po);
-            gtsam::Pose3 pGgo = gtsam::traits<gtsam::Pose3>::Compose(pG,pgo,&HG_,boost::none);
-            gtsam::Pose3 pcGgo = gtsam::traits<gtsam::Pose3>::Between(pc,pGgo,boost::none,&HGgo);
-            gtsam::Vector c = pco.transformFrom(oc);
-            gtsam::Vector C = pcGgo.transformFrom(oc,&HcGgo,boost::none);
-            gtsam::Vector dc = C-c;
+            gtsam::Pose3 pgc = gtsam::traits<gtsam::Pose3>::Between(pg,pc);
+            gtsam::Pose3 pGgc = gtsam::traits<gtsam::Pose3>::Compose(pG,pgc,&HG_,boost::none);
+            gtsam::Pose3 pcGgc = gtsam::traits<gtsam::Pose3>::Between(pc,pGgc,boost::none,&HGgc);
+            gtsam::Vector dc = pcGgc.translation(&HcGgc);
 
             if (Hg) *Hg = gtsam::Matrix16::Zero();
-            if (Ho) *Ho = gtsam::Matrix16::Zero();
             if (Hc) *Hc = gtsam::Matrix16::Zero();
 
-            if (dc[1] < 0) {
+            if (dc[1] <= 0) {
               if (HG) *HG = gtsam::Matrix16::Zero();
               return (gtsam::Vector1() << 0.0).finished();
             } else {
-              if (HG) *HG = Hy * HcGgo * HGgo * HG_;
+              if (HG) *HG = Hy * HcGgc * HGgc * HG_;
               return (gtsam::Vector1() << dc[1]).finished();
             }
+
   }
 
   /** number of variables attached to this factor */
   std::size_t size() const {
-    return 4;
+    return 3;
   }
 
 }; // \class MotionHinge
